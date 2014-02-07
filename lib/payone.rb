c25aca38120c60e5aaa0b102c95e79d96a32d3cd
asharma-ror <asharma.ror@gmail.com>
2014-02-07 13:31:22 +0530

    require spree lib files

--- lib/payone.rb
+++ lib/payone.rb
@@ -1,4 +1,22 @@
 require "payone/version"
+require 'payone/engine'
+require 'spree_core'
+
+# Require spree lib files
+require 'logger'
+require 'spree/payone/logger'
+require 'spree/payone/request_history'
+require 'spree/payone/provider/base'
+require 'spree/payone/provider/check/credit_card'
+require 'spree/payone/provider/payment/base'
+require 'spree/payone/provider/payment/credit_card'
+require 'spree/payone/provider/payment/response'
+require 'spree/payone/proxy/parameter_container'
+require 'spree/payone/proxy/response'
+require 'spree/payone/proxy/request'
+require 'spree/payone/utils/credit_card_type'
+require 'spree/payone/utils/store_card_data'
+
 
 module Payone
   # Your code goes here...
