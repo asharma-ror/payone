d6f575288e5f1117e9cd96f933b56f8b6f1bec60
asharma-ror <asharma.ror@gmail.com>
2014-02-07 15:03:26 +0530

    fix syntax issue

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,17 @@
 Spree::Core::Engine.routes.append do
+  resources :orders do
+    resource :checkout, :controller => 'checkout' do
+      member do
+        get :payone_success
+        get :payone_error
+        get :payone_back
+      end
+    end
+  end
+
+  namespace :admin do
     resources :orders do
-      resource :checkout, :controller => 'checkout' do
+      resources :payments do
         member do
           get :payone_success
           get :payone_error
@@ -9,30 +20,18 @@ Spree::Core::Engine.routes.append do
       end
     end
 
-    namespace :admin do
-      resources :orders do
-        resources :payments do
-          member do
-            get :payone_success
-            get :payone_error
-            get :payone_back
-          end
-        end
+    resource :payone_settings do
+      collection do
+        post :dismiss_alert
       end
-
-      resource :payone_settings do
-        collection do
-          post :dismiss_alert
-        end
-      end
-
-      resources :payone_logs do
-        collection do
-          get :clear
-        end
-      end
-
-      resource :payone_docs, only: :show
     end
+
+    resources :payone_logs do
+      collection do
+        get :clear
+      end
+    end
+
+    resource :payone_docs, only: :show
   end
 end
